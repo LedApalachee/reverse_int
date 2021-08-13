486f326838d09c04a3eef661d9d75ebf35a07fe6
LedApalachee <ledapalachee@gmail.com>
2021-08-13 22:35:23 +0700

    yes

--- reverse_int.c
+++ reverse_int.c
@@ -1,23 +1,21 @@
-#include <math.h>
-
 int reverse_int(int n)
 {
-	char is_negative = 0;
-	unsigned char digits_count = 1;
+	char n_is_negative = 0;
 	int result = 0;
-
 	if (n < 0)
 	{
-		is_negative = 1;
 		n *= -1;
+		n_is_negative = 1;
 	}
 
-	for (int i = 10; n / i; i *= 10)
-		++digits_count;
+	while (n / 10)
+	{
+		result += n % 10;
+		result *= 10;
+		n /= 10;
+	}
+	result += n % 10;
 
-	for (int i = digits_count-1; i >= 0; --i)
-		result += (n / (int)pow(10, i) % 10) * (int)pow(10, digits_count-1-i);
-
-	if (is_negative) result *= -1;
+	if (n_is_negative) result *= -1;
 	return result;
 }
